d3287d47493eea2391a56e5c6e9f1d012ba1b841
Sergiotachi <94398352+Sergiotachi@users.noreply.github.com>
2022-01-29 09:45:57 +0100

    suma

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -1,14 +1,5 @@
 "user strict"
 
 
-[
-    {
-      equipo: "Toros Negros",
-      puntos: [1, 3, 4, 2, 10, 8],
-    },
-    {
-      equipo: "Amanecer Dorado",
-      puntos: [8, 5, 2, 4, 7, 5, 3],
-    },
-];
+
   
