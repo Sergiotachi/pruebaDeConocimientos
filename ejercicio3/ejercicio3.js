d18142203254b8081f2c6490b38cc47183ed190e
Sergiotachi <94398352+Sergiotachi@users.noreply.github.com>
2022-01-29 08:02:52 +0100

    seguimos

---
+++ ejercicio3/ejercicio3.js
@@ -0,0 +1,27 @@
+"use strict"
+
+
+const contrasena = (max, min = 0) ; {
+    Math.round(Math.random(0, 100) * num);
+}  prompt("Introduce tu contraseña");
+
+console.log(contrasena)
+
+const  contrasena2 = prompt("Introduce tu contraseña");
+
+if (contrasena) {
+  alert("Has perdido");
+} else {
+  alert("Buenos días");
+}
+
+
+
+
+
+
+
+    
+
+
+
