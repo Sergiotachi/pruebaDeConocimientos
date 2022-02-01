807c312d2b17fea4f30e7f51de130053669ab63d
Sergiotachi <94398352+Sergiotachi@users.noreply.github.com>
2022-02-01 21:01:17 +0100

    hola

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -1,1 +1,31 @@
+"use strict"
+
+
+
+
+
+
+
+const torosNegros = [1, 3, 4, 2, 10, 8];
+const amanacerDorado = [8, 5, 2, 4, 7, 5, 3];
+let suma1 = 0;
+let suma2 = 0;
+    
+        
+    for (let i = 0; i < torosNegros.length; i++)
+     {
+           suma1 += torosNegros[i];
+
+     }
+        for (let i = 0; i < amanacerDorado.length; i++) {
+              suma2 += amanacerDorado[i];
+
+}
+
+
+
+
+console.log(suma1)
+console.log(suma2)
+
 
