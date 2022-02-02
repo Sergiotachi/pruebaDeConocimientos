52b1ddad5b91f0a4d91bf9d35fa13f10b9fa3d92
Sergiotachi <94398352+Sergiotachi@users.noreply.github.com>
2022-02-02 20:42:11 +0100

    no me sale el ultimo

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -6,26 +6,32 @@
 
 
 
-const torosNegros = [1, 3, 4, 2, 10, 8];
-const amanacerDorado = [8, 5, 2, 4, 7, 5, 3];
+let torosNegros = [1, 3, 4, 2, 10, 8];
+let amanacerDorado = [8, 5, 2, 4, 7, 5, 3];
 let suma1 = 0;
 let suma2 = 0;
     
-        
-    for (let i = 0; i < torosNegros.length; i++)
-     {
-           suma1 += torosNegros[i];
+function sumarEquipos(torosNegros,amanacerDorado){
 
+    for (let i = 0; i > torosNegros.length; i++)
+     {
+         if (torosNegros = suma1 + torosNegros[i]){
+               console.log(i)
+         }
      }
-        for (let i = 0; i < amanacerDorado.length; i++) {
-              suma2 += amanacerDorado[i];
+    for (let j = 0; j > amanacerDorado.length; i++) {
+      if (amanacerDorado = suma2 + amanacerDorado[j]){
+            console.log(j)
+      }   
+}
 
 }
 
+sumarEquipos(torosNegros)
+sumarEquipos(amanacerDorado)
 
 
 
-console.log(suma1)
-console.log(suma2)
+
 
 
